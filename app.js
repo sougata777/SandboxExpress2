f7dd986b55e1d160027686eb75a194d13a563141
Sougata <sougata777@gmail.com>
2016-01-03 23:20:08 -0600

    Tests

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 
 var app = express();
 
@@ -36,16 +37,44 @@ app.set('port', process.env.PORT || 3000);
 //    res.send('500 - Server Error');
 //});
 
+// middleware for testing
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+
+// custom middleware -- forbidder
+// define the middleware
+var forbidder = function(forbidden_day) {
+
+    var days = ['Sunday', 'Monday', 'Tueday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    return function(req, res, next) {
+
+        // get the current day
+        var day = new Date().getDay();
+
+        // check if the current day is the forbidden day
+        if (days[day] === forbidden_day) {
+            res.send('No visitors allowed on ' + forbidden_day + 's!');
+        }
+        // call the next middleware
+        else {
+            next();
+        }
+    }
+};
+app.use(forbidder('Wednesday'));
+
 // routing using handlebars templates
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
     res.render('home');
 });
 
-app.get('/about', function(req, res){
-    var randomFortune =
-        fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+app.get('/about', function(req, res) {
+    res.render('about', { fortune: fortune.getFortune() } );
 });
 
 // 404 catch-all handler (middleware)
